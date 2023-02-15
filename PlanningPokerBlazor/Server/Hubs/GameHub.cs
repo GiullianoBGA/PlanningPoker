d0d19437dcc62bdb93fd95c74e0b65d06f885718
Giulliano Souza Gonçalves <giullianosouzagoncalves@hotmail.com>
2023-02-15 07:42:04 -0300

    Criação de método para desconectar jogadores Otimização do Flip de cartas Remoção dos "valores mágicos" no meio do código

--- PlanningPokerBlazor/Server/Hubs/GameHub.cs
+++ PlanningPokerBlazor/Server/Hubs/GameHub.cs
@@ -1,62 +1,14 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using PlanningPokerBlazor.Shared;
+using System.Threading;
 
 namespace PlanningPokerBlazor.Server.Hubs
 {
     public class GameHub : Hub
     {
+        private const string DefaultCardColor = "white";
+        private const string DefaultCardValue = "0";
         private static List<GamePlay> gamePlays = new();
-
-        public async Task RegisterPlayerAsync2(RegisterPlayerRequest registerPlayerRequest)
-        {
-            var alreadyExists = gamePlays.Exists(ga => ga.HubConnectionId == Context.ConnectionId);
-
-            if (alreadyExists)
-                return;
-
-            //Atualizar dados de Context.ConnectionId com o Game recebido
-            if (RoomsCreated.RoomList.Any(x => x.GameId == registerPlayerRequest.Game.Id))
-            {
-                RoomsCreated.RoomList = RoomsCreated.RoomList.Where(x => x.GameId != null).ToList();
-            }
-            else
-            {
-                Room? room = RoomsCreated.RoomList.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
-                room.GameId = registerPlayerRequest.Game.Id;
-                room.Time = registerPlayerRequest.Game.Time;
-                room.Sequence = registerPlayerRequest.Game.Sequence;
-                room.GameType = registerPlayerRequest.Game.GameType;
-            }
-
-            if (registerPlayerRequest.Player.MemberType == EnumMemberType.Player)
-            {
-                Room roomPlayer = RoomsCreated.RoomList.First(x => x.GameId == registerPlayerRequest.Game.Id);
-
-                if (!roomPlayer.PlayerColors.Any(x => x.IsAvailable))
-                {
-                    throw new Exception("Não existem mais cadeiras disponíveis nesta sala no momento!");
-                }
-
-                registerPlayerRequest.Player.PlayerColor = GetPlayerColor(roomPlayer.PlayerColors);
-                registerPlayerRequest.Player.PlayerCSS = roomPlayer.PlayerCSSes.First(x => x.Id == registerPlayerRequest.Player.PlayerColor.Id);
-                registerPlayerRequest.Player.CardCSS = roomPlayer.CardCSSes.First(x => x.Id == registerPlayerRequest.Player.PlayerColor.Id);
-            }
-
-            var gamePlay = new GamePlay
-            {
-                HubConnectionId = Context.ConnectionId,
-                Game = registerPlayerRequest.Game,
-                Player = registerPlayerRequest.Player,
-                CardPlayed = new Card(),
-                CardVoted = new Card(),
-            };
-
-            gamePlays.Add(gamePlay);
-
-            await Groups.AddToGroupAsync(Context.ConnectionId, registerPlayerRequest.Game.Id);
-
-            await UpdateGame(gamePlay.Game.Id, registerPlayerRequest.Game.GameType);
-        }
 
         public async Task RegisterPlayerAsync(RegisterPlayerRequest registerPlayerRequest)
         {
@@ -112,33 +64,15 @@ namespace PlanningPokerBlazor.Server.Hubs
             if (!gamePlays.Exists(ga => ga.HubConnectionId == Context.ConnectionId))
                 return;
 
-            //Atualizar dados de Context.ConnectionId com o Game recebido
+            var gamePlay = gamePlays.FirstOrDefault(x => x.Game.Id == registerPlayerRequest.Game.Id && x.Player.Id == registerPlayerRequest.Player.Id);
 
-            var t = gamePlays.Where(x => x.Game.Id == registerPlayerRequest.Game.Id && x.Player.Id == registerPlayerRequest.Player.Id).ToList();
+            if (gamePlay == null) return;
 
-            Room roomPlayer = RoomsCreated.RoomList.First(x => x.GameId == registerPlayerRequest.Game.Id);
-            //if (registerPlayerRequest.Player.MemberType == EnumMemberType.Player)
-            //{
+            var room = RoomsCreated.RoomList.FirstOrDefault(x => x.GameId == gamePlay.Game.Id && gamePlay.Player.MemberType == EnumMemberType.Player);
 
-            //    if (!roomPlayer.PlayerColors.Any(x => x.IsAvailable))
-            //    {
-            //        throw new Exception("Não existem mais cadeiras disponíveis nesta sala no momento!");
-            //    }
-
-            //    registerPlayerRequest.Player.PlayerColor = GetPlayerColor(roomPlayer.PlayerColors);
-            //    registerPlayerRequest.Player.PlayerCSS = roomPlayer.PlayerCSSes.First(x => x.Id == registerPlayerRequest.Player.PlayerColor.Id);
-            //    registerPlayerRequest.Player.CardCSS = roomPlayer.CardCSSes.First(x => x.Id == registerPlayerRequest.Player.PlayerColor.Id);
-            //}
-            //            remover jogador
-
-            var gamePlay = new GamePlay
-            {
-                HubConnectionId = Context.ConnectionId,
-                Game = registerPlayerRequest.Game,
-                Player = registerPlayerRequest.Player,
-                CardPlayed = new Card(),
-                CardVoted = new Card(),
-            };
+            // Deixa a cor novamente disponivel para novos players
+            if (room != null)
+                room.PlayerColors.First(x => x.Id == gamePlay.Player.PlayerColor.Id).IsAvailable = true;
 
             gamePlays.Remove(gamePlay);
 
@@ -156,10 +90,10 @@ namespace PlanningPokerBlazor.Server.Hubs
 
             gamePlay.CardPlayed = new Card()
             {
-                Color = "white",
+                Color = DefaultCardColor,
                 Flipped = false,
-                Text = "0",
-                Value = "0"
+                Text = DefaultCardValue,
+                Value = DefaultCardValue
             };
 
             gamePlay.CardVoted = playCardRequest.CardPlayed;
@@ -174,12 +108,17 @@ namespace PlanningPokerBlazor.Server.Hubs
                 return;
             }
 
-            foreach (var gamePlay in gamePlays)
+            var options = new ParallelOptions()
             {
-                gamePlay.CardPlayed.Flipped = true;
-            }
+                MaxDegreeOfParallelism = 20
+            };
 
-            //await UpdateGame(gamePlays.First().Game.Id);
+            await Parallel.ForEachAsync(gamePlays, options, async (gamePlay, ct) =>
+            {
+                await Task.FromResult(gamePlay);
+
+                gamePlay.CardPlayed.Flipped = true;
+            });
         }
 
         public async Task ResetGame(ResetGameRequest resetGameRequest)
@@ -209,27 +148,24 @@ namespace PlanningPokerBlazor.Server.Hubs
             {
                 gamePlays.Remove(gamePlay);
                 await UpdateGame(gamePlay.Game.Id);
-            }
 
-            var room = RoomsCreated.RoomList.FirstOrDefault(x => x.GameId == gamePlay.Game.Id && gamePlay.Player.MemberType == EnumMemberType.Player);
-            if (room != null)
-                room.PlayerColors.First(x => x.Id == gamePlay.Player.PlayerColor.Id).IsAvailable = true;
+                var room = RoomsCreated.RoomList.FirstOrDefault(x => x.GameId == gamePlay.Game.Id && gamePlay.Player.MemberType == EnumMemberType.Player);
+                if (room != null)
+                    room.PlayerColors.First(x => x.Id == gamePlay.Player.PlayerColor.Id).IsAvailable = true;
 
-            if (gamePlays.Count(x => x.Game.Id == gamePlay.Game.Id) == 0)
-            {
-                if (RoomsCreated.RoomList.Count(x => x.GameId == gamePlay.Game.Id) <= 1)
-                    RoomsCreated.RoomList.Remove(RoomsCreated.RoomList.FirstOrDefault(x => x.GameId == gamePlay.Game.Id));
+                if (!gamePlays.Any(x => x.Game.Id == gamePlay.Game.Id))
+                {
+                    if (RoomsCreated.RoomList.Count(x => x.GameId == gamePlay.Game.Id) <= 1)
+                    {
+                        room = RoomsCreated.RoomList.FirstOrDefault(x => x.GameId == gamePlay.Game.Id);
+                        if (room != null)
+                            RoomsCreated.RoomList.Remove(room);
+                    }
+                }
             }
 
             await base.OnDisconnectedAsync(exception);
         }
-
-        //private async Task UpdateGame(string gameId)
-        //{
-        //    var gamesPlaysForThisGame = gamePlays.Where(gp => gp.Game.Id == gameId);
-
-        //    await Clients.Group(gameId).SendAsync("UpdateGame", gamesPlaysForThisGame);
-        //}
 
         private async Task UpdateGame(string gameId, EnumPlayerType? playerType = null)
         {
@@ -243,7 +179,7 @@ namespace PlanningPokerBlazor.Server.Hubs
             PlayerColor playerColor = new();
             if (playerColors.Count(x => x.IsAvailable) <= 1)
             {
-                playerColor = playerColors.FirstOrDefault(x => x.IsAvailable);
+                playerColor = playerColors.First(x => x.IsAvailable);
                 playerColor.IsAvailable = false;
 
                 return playerColor;
